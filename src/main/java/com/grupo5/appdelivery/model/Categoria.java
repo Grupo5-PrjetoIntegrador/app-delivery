3358d40cb63055640a64d58d22131257083ca5a6
annavighi <vighianna@gmail.com>
2024-12-10 11:41:26 -0300

    Inserção da classe model Categoria

---
+++ src/main/java/com/grupo5/appdelivery/model/Categoria.java
@@ -0,0 +1,36 @@
+package com.grupo5.appdelivery.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "tb_categoria")
+public class Categoria {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank(message = "Informe a categoria")
+	private String categoria;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(String categoria) {
+		this.categoria = categoria;
+	}
+}
